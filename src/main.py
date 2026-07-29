6413b3a90fb019abc9ec17518a6eec8656f419a0
TheBreakingPanda <marcianobl2001@gmail.com>
2026-07-29 19:38:51 +0200

    CH2 TextNode

---
+++ src/main.py
@@ -0,0 +1,13 @@
+from textnode import TextNode, TextType
+
+print("Hello, World!")
+
+def main():
+    # Create a TextNode instance
+    text_node = TextNode("Hello, World!", TextType.TEXT)
+
+    # Print the TextNode instance
+    print(text_node)
+
+
+main()
